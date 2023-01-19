81fb2e65ecc02cfbe3129171d2dca39a1092f4a5
charlesdungy <charles.dungy@gmail.com>
2023-01-18 23:25:07 -0500

    change api config

--- routes/axiosConfig.js
+++ routes/axiosConfig.js
@@ -1,16 +1,16 @@
 const axios = require('axios').default;
 
 const API_BASE_URL = process.env.API_BASE_URL;
-const API_KEY_NAME = process.env.API_KEY_NAME;
-const API_KEY_VALUE = process.env.API_KEY_VALUE;
-const API_HOST_NAME = process.env.API_HOST_NAME;
-const API_HOST_KEY_VALUE = process.env.API_HOST_KEY_VALUE;
+const API_ACCESS_KEY = process.env.API_ACCESS_KEY;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-axiosInstance.defaults.headers.common[API_KEY_NAME] = API_KEY_VALUE;
-axiosInstance.defaults.headers.common[API_HOST_NAME] = API_HOST_KEY_VALUE;
+axiosInstance.interceptors.request.use((config) => {
+  config.params = config.params || {};
+  config.params['access_key'] = API_ACCESS_KEY;
+  return config;
+});
 
 module.exports = axiosInstance;
